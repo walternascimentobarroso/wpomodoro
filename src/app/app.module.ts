7c4ffe2452d0c9fb5cc6687c97d4c1cce6e738ed
Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>
2018-07-05 15:51:35 -0400

    [ADD] New feutures add
    Signed-off-by: Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MenuBarComponent } from "./menu-bar/menu-bar.component";
 import { LayoutModule } from "@angular/cdk/layout";
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 import {
   MatToolbarModule,
   MatButtonModule,
@@ -19,6 +21,7 @@ import {
 @NgModule({
   declarations: [AppComponent, MenuBarComponent],
   imports: [
+    FlexLayoutModule,
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
